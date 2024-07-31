2384c607e737287b41f7acf22d08dfa88e6c576e
Aryan Singhal <aryansinghalmvn@gmail.com>
2024-07-31 15:33:45 +0530

    changing urls file

--- compressor/urls.py
+++ compressor/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/', views.upload_image, name='upload_image'),
+    path('', views.upload_image, name='upload_image'),
 ]
